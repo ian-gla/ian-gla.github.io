a96adc82ee47607f8590cc5b078c940a5b26a041
Ian Turton <ian.turton@glasgow.ac.uk>
2024-10-03 15:09:55 +0100

    latest version

--- getting-lost/script.js
+++ getting-lost/script.js
@@ -1,51 +1,9 @@
-function createList(div) {
-  const reasons = {
-    pedestrian: "Pedestrian Flows",
-    transport: "Transport Flows",
-    visibility: "Visibility",
-    orientation: "Self Orientation Skills",
-    context: "Personal Context",
-    amap: "Access to Reliable Map",
-    familiarity: "Familiaraty",
-    names: "Name Similarity"
-  };
-  var ul = document.createElement("ul");
-  for (const [key, value] of Object.entries(reasons)) {
-    //  generate id
-    const id = key;
 
-    // create a label
-    const label = document.createElement("label");
-    label.setAttribute("for", id);
-
-    // create a checkbox
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.name = "reason";
-    checkbox.value = key;
-    checkbox.id = id;
-
-    // place the checkbox inside a label
-    label.appendChild(checkbox);
-    // create text node
-    label.appendChild(document.createTextNode(value));
-    // add the label to the root
-    li = document.createElement("li");
-    li.appendChild(label);
-    ul.appendChild(li);
-  }
-  var target = document.querySelector("#" + div);
-  target.appendChild(ul);
-
-  ta = document.createElement("TEXTAREA");
-
-  //ta.cols = 40;
-  //ta.rows = 10;
-  ta.placeholder = "Any other information";
-  target.appendChild(ta);
-  target.style.visibility='hidden';
-}
-
+var max_dist = 1; // distance in kn points must be within
+var min_dist = 0; // distance in kn points must be beyond
+var max_angle = 90; // max angle between segments
+var pointsGood = false;
+var center = [55.872505281511444, -4.290044317503135]
 var labels = {};
 var names = {};
 labels["start"] = "Last known pos";
@@ -54,7 +12,6 @@ labels["end"] = "Next known pos";
 for (key in labels) {
   names[labels[key]] = key;
 }
-
 function createButtons(div) {
   var target = document.querySelector("#" + div);
   var startB = document.createElement("button");
@@ -77,34 +34,49 @@ function createButtons(div) {
 function clicked(e) {
   addMarker(e.target.innerHTML);
 }
-var checks = document.querySelector("#checks");
+var buttons = document.querySelector("#buttonBar");
 var info = document.querySelector("#info");
 var data_entry = document.querySelector('#data-entry-panel');
-checks.style.visibility = 'hidden';
-data_entry.style.visibility = 'hidden';
-var map = L.map("map").setView([51.505, -0.09], 13);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+var data2_entry = document.querySelector('#more-data');
+
+data_entry.style.display = 'none';
+data2_entry.style.display = 'none';
+var map = L.map("map").setView(center, 13);
+tiles = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+tiles = "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+L.tileLayer(tiles, {
   maxZoom: 19,
   attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 const search = new GeoSearch.GeoSearchControl({
   provider: new GeoSearch.OpenStreetMapProvider()
 });
 
 map.addControl(search);
-createList("checks");
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    latit = position.coords.latitude;
+    longit = position.coords.longitude;
+    // move the map to have the location in its center
+    center = [latit, longit];
+    map.panTo(new L.LatLng(latit, longit));
+    positions = {};
+  });
+}
+
+L.control.scale({"position":"bottomright"}).addTo(map);
+
 createButtons("buttonBar");
-createSubmit("checks");
 var positions = {};
 function addMarker(name) {
   const colors = {
     start:
-      "https://raw.githubusercontent.com/planetfederal/geosilk/master/silk/flag_green.png",
+    "https://raw.githubusercontent.com/planetfederal/geosilk/master/silk/flag_green.png",
     end:
-      "https://raw.githubusercontent.com/planetfederal/geosilk/master/silk/flag_red.png",
+    "https://raw.githubusercontent.com/planetfederal/geosilk/master/silk/flag_red.png",
     lost:
-      "https://raw.githubusercontent.com/planetfederal/geosilk/master/silk/flag_blue.png"
+    "https://raw.githubusercontent.com/planetfederal/geosilk/master/silk/flag_blue.png"
   };
 
   if (!positions[names[name]]) {
@@ -120,22 +92,27 @@ function addMarker(name) {
       draggable: true,
       autoPan: true
     }).addTo(map);
+    marker.on('dragend', pointsValid )
     positions[names[name]] = marker;
   } else {
     map.panTo(positions[names[name]].getLatLng());
   }
   if(Object.values(positions).length == 3){
-    displayChecks();
+    pointsValid();
+    if(pointsGood){
+      displayChecks();
+    }
   }
 }
 
-function reset(){ 
-    info.style.visibility='visible';
-    checks.style.visibility='hidden';
-    data_entry.style.visibility='hidden';
+function cleanup(){ 
+  info.style.display='block';
+  buttons.style.display = 'block';
+  data_entry.style.display='none';
+  data2_entry.style.display='none';
   var n = ['start', 'lost', 'end'];
   for(const m of n){
-    console.log(m)
+    console.log("got marker: ",m)
     console.log(positions[m])
     map.removeLayer(positions[m]);
   }
@@ -143,15 +120,69 @@ function reset(){
 
 }
 function displayChecks(){
-    info.style.visibility='hidden';
-    data_entry.style.visibility='visible';
+  console.log("info off, data 1 on")
+  buttons.style.display = 'none';
+  info.style.display='none';
+  data_entry.style.display='block';
 }
 function changeView(){
-    checks.style.visibility='visible';
-    data_entry.style.visibility='hidden';
+  data2_entry.style.display='block';
+  data_entry.style.display='none';
 }
 
-function submit() {
+function pointsValid() {
+  console.log("points check")
+  if(Object.values(positions).length != 3){
+    console.log("not enough points in check")
+    return;
+  }
+  var startPos = positions["start"] ? positions["start"].getLatLng() : "";
+  var endPos = positions["end"] ? positions["end"].getLatLng() : "";
+  var lostPos = positions["lost"] ? positions["lost"].getLatLng() : "";
+  var from = turf.point([startPos.lng, startPos.lat]);
+  var to = turf.point([endPos.lng, endPos.lat]);
+  var lost = turf.point([lostPos.lng, lostPos.lat]);
+  var options = {units: "kilometers"}
+
+  var distance1 = turf.distance(from, lost, options);
+  var distance2 = turf.distance(to, lost, options);
+  var angle = turf.angle(from, lost, to);
+  console.log("before: ",angle);
+  if(angle > 180){
+    angle=360 - angle;
+  }
+  console.log(angle);
+  var too_short = true;
+  var too_long = true;
+  var too_wide = true;
+  var message = "";
+  if (distance1 < max_dist && distance2 < max_dist){
+    too_long = false;
+  } else {
+    message += "Your points are too far apart, please adjust them.\n";
+  }
+  if (distance1 > min_dist && distance2 > min_dist){
+    too_short = false;
+  } else {
+    message += "Your points are too close together, please adjust them.\n";
+  }
+  if (angle < max_angle){
+    too_wide = false;
+  } else {
+    message += "The angle "+angle+": between your points is too wide, please adjust them.\n";
+  }
+
+
+  if (too_long || too_short || too_wide){
+    pointsGood = false;
+    alert(message);
+  } else {
+    pointsGood = true;
+    displayChecks();
+  }
+
+}
+function collectData() {
   var startPos = positions["start"] ? positions["start"].getLatLng() : "";
   var endPos = positions["end"] ? positions["end"].getLatLng() : "";
   var lostPos = positions["lost"] ? positions["lost"].getLatLng() : "";
@@ -164,19 +195,16 @@ function submit() {
   for(var i=0;i<s.length;i++){
     res[s[i].id] = s[i].options[s[i].selectedIndex].value;
   }
-  boxes = getCheckedBoxes("reason");
-  var reasons = [];
-  if (boxes) {
-    for (const box of boxes) {
-      reasons.push(box.value);
-    }
+  s= data2_entry.getElementsByTagName('select');
+  for(var i=0;i<s.length;i++){
+    res[s[i].id] = s[i].options[s[i].selectedIndex].value;
   }
-  res["reasons"] = reasons;
-  if (ta.value) {
-    res["text"] = ta.value;
+  s= data2_entry.getElementsByTagName('textarea');
+  for(var i=0;i<s.length;i++){
+    res[s[i].id] = s[i].value;
   }
   alert(JSON.stringify(res));
-  reset();
+  cleanup();
 }
 
 function getCheckedBoxes(chkboxName) {
@@ -198,6 +226,7 @@ function createSubmit(div) {
   var startEl = document.createElement("button");
   startEl.innerHTML = "submit";
   startEl.class = "button";
-  startEl.onclick = submit;
+  startEl.onclick = collectData;
   target.appendChild(startEl);
 }
+
